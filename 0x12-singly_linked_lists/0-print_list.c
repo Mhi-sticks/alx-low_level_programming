8ddc576dcc578360fa42f090719a0b16712eaccc
Daniel <adeosundaniel33@gmail.com>
2022-05-24 22:49:20 -0700

    pushyy here

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,23 @@
 #include "lists.h"
 /**
- * print_list - prints all elements of a list
- * @h: the pointer that points to the struct.
- *
- * Return: number of nodes.
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
+
 size_t print_list(const list_t *h)
 {
-	int numb_node = 0;
+	size_t nelem;
 
-	while (h != '\0')
+	nelem = 0;
+	while (h != NULL)
 	{
-		if (h->str != NULL)
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-		{
-			printf("[0] %p\n", h->str);
-		}
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		numb_node++;
+		nelem++;
 	}
-	return (numb_node);
+	return (nelem);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 /**
- * list_len - the function that return the number of elements
- * in a linked list.
- * @h: the pointer that points to the struct.
- *
- * Return: number of elements
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
+
 size_t list_len(const list_t *h)
 {
-	int elem;
+	size_t nelem;
 
-	elem = 0;
-
-	while (h != '\0')
+	nelem = 0;
+	while (h != NULL)
 	{
 		h = h->next;
-		elem++;
+		nelem++;
 	}
-	return (elem);
+	return (nelem);
 }
